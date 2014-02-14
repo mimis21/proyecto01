883dd7a40c5cf4489a85c5b722bd80d1ca14aa83
Miriam Rodriguez Rios <mrodriguezrios@gmail.com>
2014-02-14 11:22:14 -0600

    aplicacion
    aplicacion movil

---
+++ js/acciones.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+$(document).ready(function(){
+  document.addEventListener("deviceready",function(){
+$('B1').tap(function(){
+	navigator.notification.beep(1);
+	  	
+})//tap B1
+  });//device ready
+})//ready
