3dac8e88db9b38598e9186b0aa138941f8bfe6a1
Miriam Rodriguez Rios <mrodriguezrios@gmail.com>
2014-02-14 12:36:40 -0600

    CAMBIO CODIGO
    CAMBIO CODIGO

--- js/acciones.js
+++ js/acciones.js
@@ -3,7 +3,10 @@ $(document).ready(function(){
   document.addEventListener("deviceready",function(){
 $('#B1').tap(function(){
 	navigator.notification.beep(1);
-	  	
-})//tap B1
-  });//device ready
+   })//tap B1
+   $('#V1').tap(function (){//lave tap
+    navigator.notification.vibrate (500);//vibrar en el dispositivo
+   })//tap v2
+
+   });//device ready
 })//ready
